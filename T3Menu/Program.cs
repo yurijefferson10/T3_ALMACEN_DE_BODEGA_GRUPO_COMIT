5924fc6cc3801a438f7448dd54a4336803190ba6
kenny chafloc <kennychafloc08@gmail.com>
2026-06-16 01:55:39 -0500

    V1: Clas program

---
+++ T3Menu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Net.Mime;
+
+namespace T3
+
+{
+
+    class Program
+
+    {
+
+            static string [] codigos = new string [8]; 
+            static string [] nombres = new string [8]; 
+            static double [] precios = new double [8]; 
+            static int [] stock = new int [8]; 
+
+            static int cantidad = 0 ;
+    }
+}
