c68c39db71cd3fea85d7abb3ca8b5cef65d1fa3f
kenny chafloc <kennychafloc08@gmail.com>
2026-06-16 02:01:11 -0500

    V2: Static void

--- T3Menu/Program.cs
+++ T3Menu/Program.cs
@@ -16,5 +16,55 @@ namespace T3
             static int [] stock = new int [8]; 
 
             static int cantidad = 0 ;
+            static void Main (string[] args)
+
+        {
+            int opcion; 
+            do
+            {
+                Console.WriteLine("------MENU BODEGA------"); 
+                Console.WriteLine("1. Registrar el  producto"); 
+                Console.WriteLine("2. Mostrar el  productos");
+                Console.WriteLine("3. Buscar el producto"); 
+                Console.WriteLine("4. modificar el producto"); 
+                Console.WriteLine("5. insertar producto"); 
+                Console.WriteLine("6.Eliminar producto"); 
+                Console.WriteLine("7. orden  por precio ");
+                Console.WriteLine("8. Resumen ");
+                Console.WriteLine("9. Salir");
+                Console.WriteLine("Ingrese una opcion: ");
+
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1: 
+                    Registrar(); 
+                    break; 
+                    case 2:
+                    Mostrar();
+                    break;
+                    case 3:
+                    Buscar();
+                    break; 
+                    case 4: 
+                    modificar (); 
+                    break; 
+                    case 5: 
+                    Insertar();
+                    break; 
+                    case 6:
+                    Eliminar();
+                    break;
+                    case 7:
+                    Ordenar();
+                    break;
+                    case 8:
+                    Resumen();
+                    break;
+                }
+            }
+             while (opcion != 9);
+        }
     }
 }
